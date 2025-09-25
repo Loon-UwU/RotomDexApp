acd9e91720a84d374383ef3ae30f62a255250dc4
Loon-UwU <loonswitch7@gmail.com>
2025-09-25 12:45:34 -0600

    Iconos

---
+++ Core/Constant/Icons.tsx
@@ -0,0 +1,11 @@
+import Octicons from '@expo/vector-icons/Octicons';
+
+export const HomeIcon = (props) => (
+  <Octicons name="home" size={24} color="white" {...props} />
+);
+export const TeamIcon = (props) => (
+  <Octicons name="stack" size={24} color="white" {...props} />
+);
+export const ConfigIcon = (props) => (
+  <Octicons name="tools" size={24} color="white" {...props} />
+);
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,5 @@
 import { Tabs } from 'expo-router';
+import { ConfigIcon, HomeIcon, TeamIcon } from '../../Core/Constant/Icons';
 
 export default function TabsLayout() {
   return (
@@ -13,18 +14,21 @@ export default function TabsLayout() {
         name="Index"
         options={{
           title: 'Home',
+          tabBarIcon: ({ color }) => <HomeIcon color={color} />,
         }}
       />
       <Tabs.Screen
         name="Team"
         options={{
           title: 'equipo',
+          tabBarIcon: ({ color }) => <TeamIcon color={color} />,
         }}
       />
       <Tabs.Screen
         name="Config"
         options={{
           title: 'Configuracion',
+          tabBarIcon: ({ color }) => <ConfigIcon color={color}></ConfigIcon>,
         }}
       />
     </Tabs>
