636062537f404c8a4661650afd7c1531562ec8db
Loon-UwU <loonswitch7@gmail.com>
2025-09-30 01:13:41 -0600

    Informacion del pokemon

--- Core/Models/Pokemon/PokemonDataType.ts
+++ Core/Models/Pokemon/PokemonDataType.ts
@@ -1,4 +1,13 @@
 export type PokemonDataType = {
     name: string,
     id: number
+    sprites: {
+        front_default: string
+        front_shiny: string
+    }
+    types: [{
+        Slot: number
+        type: { name: string }
+
+    }]
 }
--- app/Pokemon/PokeDetails/[PokeID].tsx
+++ app/Pokemon/PokeDetails/[PokeID].tsx
@@ -1,22 +1,62 @@
 import { useLocalSearchParams } from 'expo-router';
 import { useEffect, useState } from 'react';
-import { Text } from 'react-native';
+import { FlatList, Pressable, Text, View } from 'react-native';
 import { PokemonDataType } from '../../../Core/Models/Pokemon/PokemonDataType';
 import { GetPokemonInfo } from '../../../Services/PokeApi/GetPokemonInfo';
+import { CustomImage } from '../../../components/common/CustomImage';
 
 export default function PokeDetail() {
   const { PokeID } = useLocalSearchParams();
   const [PokemonData, SetPokemonData] = useState<PokemonDataType>();
-
+  const [spriteVisible, setSpriteVisible] = useState<string | undefined>();
   useEffect(() => {
     if (PokeID) {
-      GetPokemonInfo(String(PokeID)).then(SetPokemonData);
+      GetPokemonInfo(String(PokeID)).then((data) => {
+        SetPokemonData(data);
+        setSpriteVisible(data.sprites.front_default); // Inicializa el sprite
+      });
     }
   }, [PokeID]);
 
+  function toggleSprite() {
+    if (!PokemonData) return;
+    if (spriteVisible === PokemonData.sprites.front_default)
+      setSpriteVisible(PokemonData.sprites.front_shiny);
+    else setSpriteVisible(PokemonData.sprites.front_default);
+  }
+
   return (
-    <Text>
-      {PokemonData?.name}-{PokemonData?.id}
-    </Text>
+    <View testID="Container" className=" flex-1">
+      <Pressable onPressIn={toggleSprite}>
+        <View
+          testID="Imagenes"
+          className=" items-center justify-center bg-stone-700 "
+        >
+          <CustomImage Url={spriteVisible} width={200} height={200} />
+        </View>
+      </Pressable>
+      <View testID="Types">
+        <View testID="Imagenes" className="flex-row">
+          <FlatList
+            data={PokemonData?.types ?? []}
+            numColumns={2}
+            columnWrapperStyle={{ justifyContent: 'center' }}
+            keyExtractor={(item) => String(item.Slot ?? Math.random())}
+            renderItem={({ item }) => {
+              return (
+                <View className="bg-orange-800  m-1 p-2 rounded w-28 flex items-center">
+                  <Text className="text-white ">
+                    {item.type.name.toLocaleUpperCase()}
+                  </Text>
+                </View>
+              );
+            }}
+          ></FlatList>
+        </View>
+      </View>
+      <Text>
+        {PokemonData?.name}-{PokemonData?.id}
+      </Text>
+    </View>
   );
 }
